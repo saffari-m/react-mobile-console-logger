a5032ef939c0269c953a2465916283542a24aecc
mohamadsaffari90@gmail.com <mohamadsaffari90@gmail.com>
2021-04-19 19:59:18 +0430

    add export default in IndexLayout class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 import IndexLayout from "./layouts/index";
-export { IndexLayout };
+export default IndexLayout;
 // ReactDOM.render(<IndexLayout perfix="MSF" />, document.getElementById("root"));
